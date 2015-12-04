542560c0000504c9867e0ed72e6ed5fd584d5fce
Duong Cat Hung Vuong <50903379@hcmut.edu.vn>
2015-12-05 01:53:47 +0700

    vuong modidy rao_vat 4/12

---
+++ application/views/market/left.php
@@ -0,0 +1,32 @@
+<div id="left" class="ads">
+	<div class="side-bar"> 
+		<h2><i class="fa fa-align-justify"></i>Loại</h2>
+		<div>
+			<ul>
+				<?php $results = $this->mmarket_category->get_all() ?>
+                <?php foreach($results as $row): ?>
+                    <li><a href="<?php echo site_url($row['id'].'-rao-vat-1') ?>"><span><i class="<?=$row['icon']?>"></i></span><?=$row['tenloai']?> <span class="right-icon"><!-- <i class="fa fa-arrow-circle-o-right"></i> --></span></a>
+                    </li>
+                <?php endforeach ?>
+				
+
+				
+				<li><a href="category_grid.html"><span><i class="fa fa-video-camera"></i></span>Digital Camera <span class="right-icon"><i class="fa fa-arrow-circle-o-right"></i></span></a>
+					<div class="category-mega-menu">
+						<span class="menu-text">							
+							<a href="category_grid.html">Clothing</a>
+							<a href="category_grid.html">Shoes</a>
+							<a href="category_grid.html">Bags</a>
+							<a href="category_grid.html">Headwear</a>
+							<a href="category_grid.html">Accessories</a>
+							<a href="category_grid.html">News Arrivals</a>
+						</span>
+						<span>
+							<img src="<?php echo image_url() ?>bg.png" alt="Mens" />
+						</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
